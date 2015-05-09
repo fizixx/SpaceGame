c6f64d54df4df5f68a149011c1097be26b062fe9
Tiaan Louw <tiaanl@gmail.com>
2015-05-09 12:51:03 +0200

    CMake changes to build everything together.

---
+++ cmake/junctions.cmake
@@ -0,0 +1,8 @@
+
+set(JUNCTIONS_ROOT_DIR "" CACHE PATH "Junctions source root directory")
+if(NOT JUNCTIONS_ROOT_DIR)
+  message(FATAL_ERROR "Junctions not found.  Please set JUNCTIONS_ROOT_DIR.")
+endif()
+
+add_subdirectory("${JUNCTIONS_ROOT_DIR}" "junctions")
+target_include_directories("junctions" PUBLIC "${junctions_SOURCE_DIR}/include")
